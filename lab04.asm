4b66a9e81e0936a17fa268b1b1a270f19a10783c
Barashka02 <paulnrunov@gmail.com>
2024-02-17 04:17:00 -0800

    prep points finished

--- lab04.asm
+++ lab04.asm
@@ -34,11 +34,13 @@ org	0
 	mov	tmod,#20H			; wait 1ms using T1 mode 2
 	mov	th1,#256-167	; 2MHz clock, 167 counts = 1ms
 	setb	tr1
+
 wait1:
 	jnb	tf1,wait1
 	clr	tr1						; 1ms has elapsed, stop timer
 	clr	tf1
 wait2:
+
 	mov	a,oscxcn			; now wait for crystal to stabilize
 	jnb	acc.7,wait2
 	mov	oscicn,#8			; engage! Now using 22.1184MHz
@@ -47,7 +49,119 @@ wait2:
 	mov	th1,#-6				; 9600 baud
 	setb	tr1					; start baud clock
 
-	;------------------------------------------------------------------
+msg_1:DB "It is certain", 0DH, 0AH, 0
+msg_2:DB "You may rely on it", 0DH, 0AH, 0
+msg_3:DB "Without a doubt", 0DH, 0AH,0
+msg_4:DB "Yes", 0DH, 0AH, 0
+msg_5:DB "Most Likely", 0DH, 0AH, 0
+msg_6:DB "Reply hazy, try again", 0DH, 0AH, 0
+msg_7:DB "Concentrate and ask again", 0DH, 0AH, 0
+msg_8:DB "Very Doubtful", 0DH, 0AH, 0
+msg_9:DB "My reply is no", 0DH, 0AH, 0
+;--------------------------------------------------------------------
+;MAIN
+		
+		;DESCRIPTION
+		;This is the main routine that controls the flow of the program
+;--------------------------------------------------------------------
+		
+
+
+end:
+		jmp end
+
+;--------------------------------------------------------------------
+;10 ms delay
+		
+		;DESCRIPTION
+		;This is the routine that has a 10 ms delay
+;--------------------------------------------------------------------
+
+delay:
+
+
+MOV TMOD, #01H      ; Set Timer 0 in mode 1 (16-bit mode)
+    MOV TH0, #0FCH      ; Load the high byte of the initial value (0xFC)
+    MOV TL0, #066H      ; Load the low byte of the initial value (0x66)
+
+START_TIMER:
+    SETB TR0            ; Start Timer 0
+WAIT_FOR_OVERFLOW:
+    JNB TF0, WAIT_FOR_OVERFLOW ; Wait for the overflow flag (TF0) to be set
+    CLR TF0             ; Clear the overflow flag
+    ; At this point, approximately 1 ms has elapsed.
+    ; Add your code here to do whatever needs to be done after 1 ms
+    SJMP START_TIMER    ; Loop back to start the timer again for another delay
+
+		
+
+;--------------------------------------------------------------------
+;SEND_STRING
+
+		;DESCRIPTION
+		;This subrouting sends a whole string through the serial port 
+;--------------------------------------------------------------------
+
+send_str:
+
+		clr C
+		movc	A, @A + dptr
+		jz		done			; finish this
+		call	send_char
+		inc 	dptr
+		jmp		send_str:		
+
+
+;--------------------------------------------------------------------
+;SEND CHARACTER 
+
+;  DESCRIPTION
+;	 This subroutine will take the string, and send it sends it via the serial port.  
+;--------------------------------------------------------------------
+
+send_char: 		;send a value in the acc to the serial port
+
+		mov SBUF0, A
+
+wait:
+		JNB	TI, wait
+		clr TI
+		ret
+
+;-------------------------------------------------------
+;CHECK_BUTTON
+;
+; DESCRIPTION:
+; Checks if the buttons have been pressed. Has code to 
+; protect against the button being held down. 
+;
+;-------------------------------------------------------
+chk_btn:	mov A,P2
+					cpl A
+					xch	A, old_btn
+					xrl A, old_btn
+					anl A, old_btn
+					ret
+
+; Check if the RI flag is 1?
+;--------------------------------------------------------------------
+;RANDOM
+
+		;DESCRIPTION
+		;This subroutine will take the timer and 
+
+;-------------------------------------------------------------------
+
+random:
+		call delay
+		djnz random, continue
+		mov  random. #10
+		ret 
+
+continue
+		call check_buttons.
+
+;------------------------------------------------------------------
 ;DISPLAY_LED
 ;
 ; DESCRIPTION:
